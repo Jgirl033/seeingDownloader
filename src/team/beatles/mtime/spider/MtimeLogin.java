a73a51443dcb641b8c2ff12aa326917a6dc8d748
Jgirl033 <18819423747@163.com>
2017-02-02 11:35:47 +0800

    创建时光网登录类用于登录账户

---
+++ src/team/beatles/mtime/spider/MtimeLogin.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team.beatles.mtime.spider;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+/**
+ * 时光网登录类
+ * @author admin Jgirl
+ */
+public class MtimeLogin {
+
+    /**
+     * 启动chrome浏览器，用于手动登录时光网账号
+     * @return WebDriver 
+     */
+    public static WebDriver getWebDriver() {
+        File file = new File("exe\\chromedriver.exe"); //chromediriver的指定目录
+
+        ChromeDriverService service = new ChromeDriverService.Builder().usingDriverExecutable(file).usingAnyFreePort().build();
+        try {
+            service.start();
+        } catch (IOException ex) {
+            Logger.getLogger(MtimeLogin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        WebDriver dr = new RemoteWebDriver(service.getUrl(), DesiredCapabilities.chrome());
+        dr.get("http://movie.mtime.com/");
+        try {
+            Thread.sleep(30000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(MtimeLogin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return dr;
+    }
+}
