7b7e5899df195aecf8b962fd5c70d1f78212cfa3
Jgirl033 <18819423747@163.com>
2017-02-08 21:24:50 +0800

    添加selenium报Timeout错误即继续尝试访问网页，直到成功。

--- src/team/beatles/douban/collector/DoubanUserPageCollector.java
+++ src/team/beatles/douban/collector/DoubanUserPageCollector.java
@@ -8,6 +8,7 @@ package team.beatles.douban.collector;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.openqa.selenium.TimeoutException;
 import team.beatles.downloader.file.Writer;
 import static team.beatles.constant.Constant.doubanDR;
 
@@ -31,22 +32,30 @@ public class DoubanUserPageCollector {
      * @param uidList 豆瓣用户ID列表
      */
     public void start(ArrayList<String> uidList) {
+        try {
+            for (String uid : uidList) {
 
-        for (String uid : uidList) {
+                String userUrl = "https://www.douban.com/people/" + uid + "/";
+                while (true) {
+                    try {
+                        doubanDR.get(userUrl);
+                        break;
+                    } catch (TimeoutException e) {
+                        System.out.println(e.toString());
+                    }
+                }
 
-            String userUrl = "https://www.douban.com/people/" + uid + "/";
-            doubanDR.get(userUrl);
-            String sourceCode = doubanDR.getPageSource().replaceAll("\n", "");//使用浏览器获取的网页源代码中含有换行符，需要过滤掉;
-            Writer w = new Writer("doc/client/douban/user/", uid + ".txt");
-            w.write(sourceCode);
+                String sourceCode = doubanDR.getPageSource().replaceAll("\n", "");//使用浏览器获取的网页源代码中含有换行符，需要过滤掉;
+                Writer w = new Writer("doc/client/douban/user/", uid + ".txt");
+                w.write(sourceCode);
 
-            java.util.Random random = new java.util.Random();// 定义随机类
-            int result = random.nextInt(3000) + 6000;// 返回[0,10)集合中的整数，注意不包括10
-            try {
+                java.util.Random random = new java.util.Random();// 定义随机类
+                int result = random.nextInt(3000) + 6000;// 返回[0,10)集合中的整数，注意不包括10
+
                 Thread.sleep(result);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(DoubanUserPageCollector.class.getName()).log(Level.SEVERE, null, ex);
             }
+        } catch (InterruptedException ex) {
+            Logger.getLogger(DoubanUserPageCollector.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
