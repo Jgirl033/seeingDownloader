5edc92ddfd3715e1b1e068aca9400879a9fb5598
Jgirl033 <18819423747@163.com>
2017-02-11 14:23:21 +0800

    修改获取电影相关获奖信息的方法，首先截取获奖情况区域的源代码，再进行解析，以缩小正则匹配时间。同时对电影原码的解析进行拆分，实践证明这样可以加快解析速度

--- src/team/beatles/douban/spider/DoubanMovieSpider.java
+++ src/team/beatles/douban/spider/DoubanMovieSpider.java
@@ -22,7 +22,7 @@ public class DoubanMovieSpider {
 
     private final String mid;
     private final DoubanMovie movie;
-    private final String sourceCode;
+    private final String informationSourceCode;
 
     /**
      * 使用豆瓣电影ID构造一个新的DoubanMovieSpider()
@@ -32,12 +32,12 @@ public class DoubanMovieSpider {
     public DoubanMovieSpider(String mid) {
         this.movie = new DoubanMovie();
         this.mid = mid;
- 
+
         String filename = mid + ".txt";
         System.out.println("该电影的源代码路径是：" + filename);
         Reader r = new Reader("doc/server/douban/movie/", filename);
-        this.sourceCode = r.read();
-       
+        this.informationSourceCode = r.read();
+
     }
 
     /**
@@ -45,61 +45,113 @@ public class DoubanMovieSpider {
      *
      * @return Movie 某部电影的基本信息
      */
-    public DoubanMovie getMovie(){
+    public DoubanMovie getMovie() {
 
-        Pattern patternInfomation = Pattern.compile("<title>(.*?)</title>.*?<span class='pl'>导演</span>:(.*?)</span><br/>.*?<span class='pl'>编剧</span>:(.*?)</span><br/>.*?<span class='pl'>主演</span>:(.*?)</span><br/>.*?<span class=\"pl\">类型:(.*?)</span><br/>.*?<span class=\"pl\">制片国家/地区:(.*?)<br/>.*?<span class=\"pl\">语言:</span>(.*?)<br/>.*?<span class=\"pl\">上映日期:(.*?)</span><br/>.*?<span class=\"pl\">片长:</span>.*?<span property=\"v:runtime\".*?>(.*?)分钟.*?</span>.*?v:summary.*?>(.*?)</span>");
-        Matcher matcherInformation = patternInfomation.matcher(this.sourceCode);
-        if (matcherInformation.find()) {
-            System.out.println(matcherInformation.group(1));
-            movie.setMid(Integer.parseInt(this.mid));
-            movie.setName(matcherInformation.group(1).replaceAll("<.*?>", "").replace("(豆瓣)", "").trim());
-            movie.setImgSrc(this.getImage());
-            movie.setDirector(matcherInformation.group(2).replaceAll("<.*?>", "").trim());
-            movie.setScreenwriter(matcherInformation.group(3).replaceAll("<.*?>", "").trim());
-            movie.setPerformer(matcherInformation.group(4).replaceAll("<.*?>", "").trim());
-            movie.setStyle(matcherInformation.group(5).replaceAll("<.*?>", "").trim());
-            movie.setArea(matcherInformation.group(6).replaceAll("<.*?>", "").trim());
-            movie.setLanguage(matcherInformation.group(7).replaceAll("<.*?>", "").trim());
-            movie.setReleaseTime(matcherInformation.group(8).replaceAll("<.*?>", "").trim());
-            movie.setRuntime(matcherInformation.group(9).replaceAll("<.*?>", "").trim());
-            movie.setSynopsis(matcherInformation.group(10).replaceAll("<.*?>", "").trim());
-            String award = "";
-            for (String item : this.getAwardsSituation()) {
-                award += item.replaceAll("<.*?>", "").trim() + "#";
-            }
-            movie.setAward(award);
-            String like = "";
-            for (String item : this.getRecommendation()) {
-                like += item.replaceAll("<.*?>", "").trim() + "#";
-            }
-            movie.setLike(like);
-            movie.setJsonSrc("./" + this.mid);
+        movie.setMid(Integer.parseInt(this.mid));
 
-            Pattern patternScore = Pattern.compile("<.*?rating_num.*?>(.*?)</strong>.*?v:votes.*?>(.*?)</span>.*?title=\"力荐.*?rating_per\">(.*?)%</span>.*?title=\"推荐.*?rating_per\">(.*?)%</span>.*?title=\"还行.*?rating_per\">(.*?)%</span>.*?title=\"较差.*?rating_per\">(.*?)%</span>.*?title=\"很差.*?rating_per\">(.*?)%</span>");
-            Matcher matcherScore = patternScore.matcher(this.sourceCode);
-            if (matcherScore.find()) {
-                movie.setRating(Double.valueOf(matcherScore.group(1).trim()));
-                movie.setEvaluationNumber(Integer.valueOf(matcherScore.group(2).trim()));
-                movie.setFive(Double.valueOf(matcherScore.group(3).trim()));
-                movie.setFour(Double.valueOf(matcherScore.group(4).trim()));
-                movie.setThree(Double.valueOf(matcherScore.group(5).trim()));
-                movie.setTwo(Double.valueOf(matcherScore.group(6).trim()));
-                movie.setOne(Double.valueOf(matcherScore.group(7).trim()));
-            } else {
-                System.out.println("高能预警：正则表达式匹配不正确！");
-            }
+        Pattern patternName = Pattern.compile("<title>(.*?)</title>");
+        Matcher matcherName = patternName.matcher(this.informationSourceCode);
+        if (matcherName.find()) {
+            movie.setName(matcherName.group(1).replaceAll("<.*?>", "").replace("(豆瓣)", "").trim());
+        }
+
+        movie.setImgSrc(this.getImage());
+
+        Pattern patternDirector = Pattern.compile("<span class='pl'>导演</span>:(.*?)</span>");
+        Matcher matcherDirector = patternDirector.matcher(this.informationSourceCode);
+        if (matcherDirector.find()) {
+            movie.setDirector(matcherDirector.group(1).replaceAll("<.*?>", "").trim());
+        }
+
+        Pattern patternScreenWriter = Pattern.compile("<span class='pl'>编剧</span>:(.*?)</span>");
+        Matcher matcherScreenWriter = patternScreenWriter.matcher(this.informationSourceCode);
+        if (matcherScreenWriter.find()) {
+            movie.setScreenwriter(matcherScreenWriter.group(1).replaceAll("<.*?>", "").trim());
+        }
+
+        Pattern patternPerformance = Pattern.compile("<span class='pl'>主演</span>:(.*?)</span>");
+        Matcher matcherPerformance = patternPerformance.matcher(this.informationSourceCode);
+        String performance = "";
+        if (matcherPerformance.find()) {
+            performance = matcherPerformance.group(1).replaceAll("<.*?>", "").trim();
+        }
+        movie.setPerformer(performance);
+
+        Pattern patternStyle = Pattern.compile("<span class=\"pl\">类型:(.*?)</span>");
+        Matcher matcherStyle = patternStyle.matcher(this.informationSourceCode);
+        if (matcherStyle.find()) {
+            movie.setArea(matcherStyle.group(1).replaceAll("<.*?>", "").trim());
+        }
+
+        Pattern patternArea = Pattern.compile("<span class=\"pl\">制片国家/地区:(.*?)<br/>");
+        Matcher matcherArea = patternArea.matcher(this.informationSourceCode);
+        if (matcherArea.find()) {
+            movie.setArea(matcherArea.group(1).replaceAll("<.*?>", "").trim());
+        }
+
+        Pattern patternLanguage = Pattern.compile("<span class=\"pl\">语言:</span>(.*?)<br/>");
+        Matcher matcherLanguage = patternLanguage.matcher(this.informationSourceCode);
+        if (matcherLanguage.find()) {
+            movie.setSynopsis(matcherLanguage.group(1).replaceAll("<.*?>", "").trim());
+        }
+
+        Pattern patternReleaseTime = Pattern.compile("<span class=\"pl\">上映日期:(.*?)</span>");
+        Matcher matcherReleaseTime = patternReleaseTime.matcher(this.informationSourceCode);
+        if (matcherReleaseTime.find()) {
+            movie.setSynopsis(matcherReleaseTime.group(1).replaceAll("<.*?>", "").trim());
+        }
+
+        Pattern patternRunTime = Pattern.compile("<span class=\"pl\">片长:</span>.*?<span property=\"v:runtime\".*?>(.*?)分钟.*?</span>");
+        Matcher matcherRunTime = patternRunTime.matcher(this.informationSourceCode);
+        if (matcherRunTime.find()) {
+            movie.setSynopsis(matcherRunTime.group(1).replaceAll("<.*?>", "").trim());
+        }
+
+        Pattern patternSynopsis = Pattern.compile("v:summary.*?>(.*?)</span>");
+        Matcher matcherSynopsis = patternSynopsis.matcher(this.informationSourceCode);
+        if (matcherSynopsis.find()) {
+            movie.setSynopsis(matcherSynopsis.group(1).replaceAll("<.*?>", "").trim());
+        }
+
+        String award = "";
+        for (String item : this.getAwardsSituation()) {
+            award += item.replaceAll("<.*?>", "").trim() + "#";
+        }
+        movie.setAward(award);
+
+        String like = "";
+        for (String item : this.getRecommendation()) {
+            like += item.replaceAll("<.*?>", "").trim() + "#";
+        }
+        movie.setLike(like);
+
+        movie.setJsonSrc("./" + this.mid);
+
+        Pattern patternScore = Pattern.compile("<.*?rating_num.*?>(.*?)</strong>.*?v:votes.*?>(.*?)</span>.*?title=\"力荐.*?rating_per\">(.*?)%</span>.*?title=\"推荐.*?rating_per\">(.*?)%</span>.*?title=\"还行.*?rating_per\">(.*?)%</span>.*?title=\"较差.*?rating_per\">(.*?)%</span>.*?title=\"很差.*?rating_per\">(.*?)%</span>");
+        Matcher matcherScore = patternScore.matcher(this.informationSourceCode);
+        if (matcherScore.find()) {
+            movie.setRating(Double.valueOf(matcherScore.group(1).trim()));
+            movie.setEvaluationNumber(Integer.valueOf(matcherScore.group(2).trim()));
+            movie.setFive(Double.valueOf(matcherScore.group(3).trim()));
+            movie.setFour(Double.valueOf(matcherScore.group(4).trim()));
+            movie.setThree(Double.valueOf(matcherScore.group(5).trim()));
+            movie.setTwo(Double.valueOf(matcherScore.group(6).trim()));
+            movie.setOne(Double.valueOf(matcherScore.group(7).trim()));
+        } else {
+            System.out.println("高能预警：解析电影评价情况的正则表达式匹配不正确！");
         }
         return this.movie;
     }
 
     /**
      * 获取某部电影的海报链接
+     *
      * @return String 某部电影的海报路径
      */
     public String getImage() {
         String imagePath = "";
         Pattern pattern = Pattern.compile("<a class=\".*?\" href=\".*?\" title=\"点击看更多海报\">.*?<img src=\"(.*?)\" title=\"点击看更多海报\" alt=\".*?\" rel=\"v:image\" />.*?</a>");
-        Matcher matcher = pattern.matcher(this.sourceCode);
+        Matcher matcher = pattern.matcher(this.informationSourceCode);
         if (matcher.find()) {
             imagePath = matcher.group(1);
         }
@@ -108,12 +160,13 @@ public class DoubanMovieSpider {
 
     /**
      * 获取看过某部电影的用户也喜欢看的相关电影
+     *
      * @return ArrayList 电影获奖情况列表
      */
     public ArrayList<String> getRecommendation() {
         ArrayList<String> recommendation = new ArrayList<>();
         Pattern pattern = Pattern.compile("<div class=\"recommendations-bd\">(.*?)</div>");
-        Matcher matcher = pattern.matcher(this.sourceCode);
+        Matcher matcher = pattern.matcher(this.informationSourceCode);
         if (matcher.find()) {
             String tmp = matcher.group(1);
             pattern = Pattern.compile("<a href.*?>.*?<img.*?alt=\"(.*?)\"");
@@ -127,14 +180,27 @@ public class DoubanMovieSpider {
 
     /**
      * 获取某部电影的获奖情况
+     *
      * @return ArrayList 电影获奖情况列表
      */
     public ArrayList<String> getAwardsSituation() {
         String url = "https://movie.douban.com/subject/" + this.mid + "/awards/";
         ArrayList<String> awards = new ArrayList<>();
         WebPage awardsPage = new WebConnect(url).downloadPage();
+
+        String awardsSourceCode = awardsPage.getSourceCode();
+        Pattern patternContent = Pattern.compile("<div id=\"content\">(.*?)</div>");
+        Matcher matcherContent = patternContent.matcher(awardsSourceCode);
+
+        String content;
+        if (matcherContent.find()) {
+            content = matcherContent.group(1);
+        }else{
+            content = "";
+        }
+        
         Pattern pattern = Pattern.compile("<ul class=\"award\">.*?<li>(.*?)</li>.*?<li>(.*?)</li>.*?</ul>");
-        Matcher matcher = pattern.matcher(awardsPage.getSourceCode());
+        Matcher matcher = pattern.matcher(content);
         while (matcher.find()) {
             awards.add(matcher.group(1).replaceAll("<.*?>", "").trim() + ":" + matcher.group(2).replaceAll("<.*?>", "").trim());
         }
