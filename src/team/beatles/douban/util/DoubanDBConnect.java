1d96a07d8546e19895d2de17a3420a6f0c426770
Jgirl033 <18819423747@163.com>
2017-02-23 21:22:22 +0800

    修改数据库连接字符串中的IP值为远程IP

--- src/team/beatles/douban/util/DoubanDBConnect.java
+++ src/team/beatles/douban/util/DoubanDBConnect.java
@@ -22,7 +22,7 @@ class DoubanDBConnect {
         try {
             //加载驱动，这一句也可写为：Class.forName("com.mysql.jdbc.Driver");
             Class.forName("com.mysql.jdbc.Driver").newInstance();
-            this.conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/downloader?zeroDateTimeBehavior=convertToNull", "root", "gdufsiiip");
+            this.conn = DriverManager.getConnection("jdbc:mysql://192.168.235.20:3306/downloader?characterEncoding=utf-8", "root", "iiip");
             System.out.println("连接成功！");
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
             System.out.println("混蛋 : " + ex.toString());
